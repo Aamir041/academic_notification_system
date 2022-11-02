5764e3ba33688ba72269be954fef7dd53cd0100e
Aamir041 <aamirsaudagar34@gmail.com>
2022-11-02 15:07:19 +0530

    added

--- app/Dashboard.js
+++ app/Dashboard.js
@@ -5,16 +5,15 @@ import WeeklyCalendar from 'react-native-weekly-calendar';
 export const Dashboard = () => {
   const sampleEvents = [
     { 'start': '2022-10-30 09:00:00', 'duration': '00:20:00', 'note': 'Mobile Communication Assignment Submission' },
-    { 'start': '2022-10-31 14:00:00', 'duration': '01:00:00', 'note': 'Doctor\'s appointment' },
-    { 'start': '2022-11-01 08:00:00', 'duration': '00:30:00', 'note': 'Morning exercise' },
-    { 'start': '2022-11-01 14:00:00', 'duration': '02:00:00', 'note': 'Meeting with client' },
-    { 'start': '2022-11-02 19:00:00', 'duration': '01:00:00', 'note': 'Dinner with family' },
-    { 'start': '2022-11-02 09:30:00', 'duration': '01:00:00', 'note': 'Schedule 1' },
-    { 'start': '2022-11-03 11:00:00', 'duration': '02:00:00', 'note': 'Schedule 2' },
-    { 'start': '2022-11-03 15:00:00', 'duration': '01:30:00', 'note': 'Schedule 3' },
-    { 'start': '2022-11-04 18:00:00', 'duration': '02:00:00', 'note': 'Schedule 4' },
-    { 'start': '2022-11-04 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' },
-    { 'start': '2022-11-05 22:00:00', 'duration': '01:00:00', 'note': 'Hello World' },
+    { 'start': '2022-10-31 14:00:00', 'duration': '01:00:00', 'note': 'Apple Ideathon' },
+    { 'start': '2022-11-01 08:00:00', 'duration': '00:30:00', 'note': 'GDSC App Development Bootcamp' },
+    { 'start': '2022-11-01 14:00:00', 'duration': '02:00:00', 'note': 'Session On Startup' },
+    { 'start': '2022-11-02 19:00:00', 'duration': '01:00:00', 'note': 'Human Library Event' },
+    { 'start': '2022-11-02 03:03:00', 'duration': '01:00:00', 'note': 'Mini Project Presentation' },
+    { 'start': '2022-11-03 11:00:00', 'duration': '02:00:00', 'note': 'TOC Urgent Tutorial' },
+    { 'start': '2022-11-03 15:00:00', 'duration': '01:30:00', 'note': 'Talk On Auyrveda' },
+    { 'start': '2022-11-04 18:00:00', 'duration': '02:00:00', 'note': 'SAD Presentation' },
+    { 'start': '2022-11-05 22:00:00', 'duration': '01:00:00', 'note': 'IGNITE Debate Competition' },
   ]
 
   return (
